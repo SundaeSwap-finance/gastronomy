a5d30ca5a4ce786683cfa77d01f4c32918a932f5
rrruko <rukokarasu@gmail.com>
2024-03-13 14:30:58 -0700

    demonstrate dependency on aiken uplc crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use uplc::machine::value::{Value};
+use uplc::ast::{Constant};
+
 fn main() {
-    println!("Hello, world!");
+    let n = Value::Con(Constant::Integer(1.into()).into());
+    println!("Hello, world! {:?}", n);
 }
