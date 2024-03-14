8a7cccadc17a66e071ad3b7416dc9471218947e6
Pi Lanningham <pi.lanningham@gmail.com>
2024-03-13 20:01:38 -0400

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use uplc::machine::value::{Value};
-use uplc::ast::{Constant};
+use uplc::ast::Constant;
+use uplc::machine::value::Value;
 
 fn main() {
     let n = Value::Con(Constant::Integer(1.into()).into());
