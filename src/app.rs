37e576a27c57a71a7194926002bcb567f35c7224
Shaakeed Belizaire <shaakeed@sundaeswap.finance>
2024-03-20 14:46:35 -0400

    first step in breaking up render

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
 use std::io::{self};
 use std::path::PathBuf;
+use std::rc::Rc;
 
 use crate::utils;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
@@ -8,8 +9,10 @@ use ratatui::{
     symbols::border,
     widgets::{block::*, *},
 };
+use uplc::ast::{NamedDeBruijn, Term};
 use uplc::machine::cost_model::ExBudget;
-use uplc::machine::MachineState;
+use uplc::machine::value::Value;
+use uplc::machine::{Context, MachineState};
 
 #[derive(Default)]
 pub struct App {
@@ -122,15 +125,12 @@ impl Widget for &mut App {
             .split(block.inner(area));
 
         block.render(area, buf);
+        
         let gauge_region = layout[0];
         let command_region = layout[1];
         let main_region = layout[2];
 
-        Gauge::default()
-            .gauge_style(Style::default().fg(Color::Green))
-            .label(format!("Step {}/{}", self.cursor, self.states.len() - 1))
-            .ratio(self.cursor as f64 / self.states.len() as f64)
-            .render(gauge_region, buf);
+        render_gauge_region(self.cursor, &self.states, gauge_region, buf);
 
         let layout = Layout::default()
             .direction(Direction::Horizontal)
@@ -181,15 +181,8 @@ impl Widget for &mut App {
                 }
             }
         };
-        let next = if self.cursor < self.states.len() - 1 {
-            match &self.states[self.cursor + 1].0 {
-                MachineState::Compute(_, _, _) => "Compute",
-                MachineState::Return(_, _) => "Return",
-                MachineState::Done(_) => "Done",
-            }
-        } else {
-            "None"
-        };
+
+        let next = get_next(self.cursor, &self.states);
 
         let (cpu, mem) = (10000000000 - curr_state.1.cpu, 14000000 - curr_state.1.mem);
         let (prev_cpu, prev_mem) = if self.cursor > 0 {
@@ -199,126 +192,213 @@ impl Widget for &mut App {
             (0, 0)
         };
 
-        Line::from(vec![
-            "Current: ".into(),
-            label.fg(Color::Blue).add_modifier(Modifier::BOLD),
-        ])
-        .left_aligned()
-        .render(command_region, buf);
-        Line::from(vec![
-            "Budget: ".into(),
-            format!("{} steps ", cpu)
-                .fg(Color::Blue)
-                .add_modifier(Modifier::BOLD),
-            if prev_cpu < cpu {
-                format!("(+{}) ", cpu - prev_cpu).fg(Color::Green)
-            } else {
-                "".into()
-            },
-            format!("{} mem ", mem)
-                .fg(Color::Blue)
-                .add_modifier(Modifier::BOLD),
-            if prev_mem < mem {
-                format!("(+{}) ", mem - prev_mem).fg(Color::Green)
-            } else {
-                "".into()
-            },
-        ])
-        .centered()
-        .render(command_region, buf);
-        Line::from(vec![
-            "Next: ".into(),
-            next.fg(Color::Blue).add_modifier(Modifier::ITALIC),
-        ])
-        .right_aligned()
-        .render(command_region, buf);
+        render_command_region(
+            cpu,
+            mem,
+            prev_cpu,
+            prev_mem,
+            buf,
+            next,
+            command_region,
+            label,
+        );
 
-        let term_block = Block::default()
-            .title(" Term ".fg(if self.focus == "Term" {
-                Color::Blue
-            } else {
-                Color::Reset
-            }))
-            .borders(Borders::TOP | Borders::LEFT | Borders::BOTTOM)
+        render_term_region(self.focus.clone(), term, self.term_scroll, term_region, buf);
+
+        render_context_region(
+            self.focus.clone(),
+            context,
+            context_region,
+            self.context_scroll,
+            buf,
+        );
+
+        render_env_region(env, self.focus.clone(), self.env_scroll, env_region, buf);
+
+        render_clear_popup_region(area, ret_value, buf);
+    }
+}
+
+fn render_gauge_region(
+    cursor: usize,
+    states: &Vec<(MachineState, ExBudget)>,
+    gauge_region: Rect,
+    buf: &mut Buffer,
+) {
+    Gauge::default()
+        .gauge_style(Style::default().fg(Color::Green))
+        .label(format!("Step {}/{}", cursor, states.len() - 1))
+        .ratio(cursor as f64 / states.len() as f64)
+        .render(gauge_region, buf);
+}
+
+fn get_next(cursor: usize, states: &Vec<(MachineState, ExBudget)>) -> &str {
+    if cursor < states.len() - 1 {
+        match &states[cursor + 1].0 {
+            MachineState::Compute(_, _, _) => return "Compute",
+            MachineState::Return(_, _) => return "Return",
+            MachineState::Done(_) => return "Done",
+        }
+    } else {
+        return "None";
+    };
+}
+
+fn render_command_region(
+    cpu: i64,
+    mem: i64,
+    prev_cpu: i64,
+    prev_mem: i64,
+    buf: &mut Buffer,
+    next: &str,
+    command_region: Rect,
+    label: &str,
+) {
+    Line::from(vec![
+        "Current: ".into(),
+        label.fg(Color::Blue).add_modifier(Modifier::BOLD),
+    ])
+    .left_aligned()
+    .render(command_region, buf);
+    Line::from(vec![
+        "Budget: ".into(),
+        format!("{} steps ", cpu)
+            .fg(Color::Blue)
+            .add_modifier(Modifier::BOLD),
+        if prev_cpu < cpu {
+            format!("(+{}) ", cpu - prev_cpu).fg(Color::Green)
+        } else {
+            "".into()
+        },
+        format!("{} mem ", mem)
+            .fg(Color::Blue)
+            .add_modifier(Modifier::BOLD),
+        if prev_mem < mem {
+            format!("(+{}) ", mem - prev_mem).fg(Color::Green)
+        } else {
+            "".into()
+        },
+    ])
+    .centered()
+    .render(command_region, buf);
+    Line::from(vec![
+        "Next: ".into(),
+        next.fg(Color::Blue).add_modifier(Modifier::ITALIC),
+    ])
+    .right_aligned()
+    .render(command_region, buf);
+}
+
+fn render_term_region(
+    focus: String,
+    term: &Term<NamedDeBruijn>,
+    mut term_scroll: u16,
+    term_region: Rect,
+    buf: &mut Buffer,
+) {
+    let term_block = Block::default()
+        .title(" Term ".fg(if focus == "Term" {
+            Color::Blue
+        } else {
+            Color::Reset
+        }))
+        .borders(Borders::TOP | Borders::LEFT | Borders::BOTTOM)
+        .border_set(border::PLAIN);
+
+    let term_text = term.to_string();
+    let max_term_scroll = term_text.split('\n').count() as u16 - 1;
+    if term_scroll > max_term_scroll {
+        term_scroll = max_term_scroll;
+    }
+
+    Paragraph::new(term_text)
+        .block(term_block)
+        .scroll((term_scroll, 0))
+        .render(term_region, buf);
+}
+
+fn render_context_region(
+    focus: String,
+    context: &Context,
+    context_region: Rect,
+    mut context_scroll: u16,
+    buf: &mut Buffer,
+) {
+    let top_right_border_set = symbols::border::Set {
+        top_left: symbols::line::NORMAL.horizontal_down,
+        ..symbols::border::PLAIN
+    };
+    let context_block = Block::default()
+        .title(" Context ".fg(if focus == "Context" {
+            Color::Blue
+        } else {
+            Color::Reset
+        }))
+        .borders(Borders::TOP | Borders::LEFT | Borders::RIGHT)
+        .border_set(top_right_border_set);
+
+    let context_text = utils::context_to_string(context.clone());
+    let max_context_scroll = context_text.split('\n').count() as u16 - 1;
+    if context_scroll > max_context_scroll {
+        context_scroll = max_context_scroll;
+    }
+
+    Paragraph::new(context_text)
+        .block(context_block)
+        .scroll((context_scroll, 0))
+        .render(context_region, buf);
+}
+
+fn render_env_region(
+    env: &Rc<Vec<uplc::machine::value::Value>>,
+    focus: String,
+    mut env_scroll: u16,
+    env_region: Rect,
+    buf: &mut Buffer,
+) {
+    let collapsed_top_and_left_border_set = symbols::border::Set {
+        top_left: symbols::line::NORMAL.vertical_right,
+        top_right: symbols::line::NORMAL.vertical_left,
+        bottom_left: symbols::line::NORMAL.horizontal_up,
+        ..symbols::border::PLAIN
+    };
+    let env_block = Block::default()
+        .title(" Env ".fg(if focus == "Env" {
+            Color::Blue
+        } else {
+            Color::Reset
+        }))
+        .borders(Borders::ALL)
+        .border_set(collapsed_top_and_left_border_set);
+
+    let env_text = utils::env_to_string(env);
+    let max_env_scroll = env_text.split('\n').count() as u16 - 1;
+    if env_scroll > max_env_scroll {
+        env_scroll = max_env_scroll;
+    }
+
+    Paragraph::new(env_text)
+        .block(env_block)
+        .scroll((env_scroll, 0))
+        .render(env_region, buf);
+}
+
+fn render_clear_popup_region(area: Rect, ret_value: Option<&Value>, buf: &mut Buffer) {
+    if let Some(value) = ret_value {
+        let ret_block = Block::default()
+            .title(" Return Value ")
+            .borders(Borders::ALL)
             .border_set(border::PLAIN);
 
-        let term_text = term.to_string();
-        let max_term_scroll = term_text.split('\n').count() as u16 - 1;
-        if self.term_scroll > max_term_scroll {
-            self.term_scroll = max_term_scroll;
-        }
-
-        Paragraph::new(term_text)
-            .block(term_block)
-            .scroll((self.term_scroll, 0))
-            .render(term_region, buf);
-
-        let top_right_border_set = symbols::border::Set {
-            top_left: symbols::line::NORMAL.horizontal_down,
-            ..symbols::border::PLAIN
+        let popup_area = Rect {
+            x: area.width / 4,
+            y: area.height / 3,
+            width: area.width / 2,
+            height: area.height / 3,
         };
-        let context_block = Block::default()
-            .title(" Context ".fg(if self.focus == "Context" {
-                Color::Blue
-            } else {
-                Color::Reset
-            }))
-            .borders(Borders::TOP | Borders::LEFT | Borders::RIGHT)
-            .border_set(top_right_border_set);
-
-        let context_text = utils::context_to_string(context.clone());
-        let max_context_scroll = context_text.split('\n').count() as u16 - 1;
-        if self.context_scroll > max_context_scroll {
-            self.context_scroll = max_context_scroll;
-        }
-
-        Paragraph::new(context_text)
-            .block(context_block)
-            .scroll((self.context_scroll, 0))
-            .render(context_region, buf);
-
-        let collapsed_top_and_left_border_set = symbols::border::Set {
-            top_left: symbols::line::NORMAL.vertical_right,
-            top_right: symbols::line::NORMAL.vertical_left,
-            bottom_left: symbols::line::NORMAL.horizontal_up,
-            ..symbols::border::PLAIN
-        };
-        let env_block = Block::default()
-            .title(" Env ".fg(if self.focus == "Env" {
-                Color::Blue
-            } else {
-                Color::Reset
-            }))
-            .borders(Borders::ALL)
-            .border_set(collapsed_top_and_left_border_set);
-
-        let env_text = utils::env_to_string(env);
-        let max_env_scroll = env_text.split('\n').count() as u16 - 1;
-        if self.env_scroll > max_env_scroll {
-            self.env_scroll = max_env_scroll;
-        }
-
-        Paragraph::new(env_text)
-            .block(env_block)
-            .scroll((self.env_scroll, 0))
-            .render(env_region, buf);
-
-        if let Some(value) = ret_value {
-            let ret_block = Block::default()
-                .title(" Return Value ")
-                .borders(Borders::ALL)
-                .border_set(border::PLAIN);
-
-            let popup_area = Rect {
-                x: area.width / 4,
-                y: area.height / 3,
-                width: area.width / 2,
-                height: area.height / 3,
-            };
-            Clear.render(popup_area, buf);
-            Paragraph::new(uplc::machine::discharge::value_as_term(value.clone()).to_string())
-                .block(ret_block)
-                .render(popup_area, buf);
-        }
+        Clear.render(popup_area, buf);
+        Paragraph::new(uplc::machine::discharge::value_as_term(value.clone()).to_string())
+            .block(ret_block)
+            .render(popup_area, buf);
     }
 }
