5da85a0be1fd9dfed3e0558592c151cd2e79b29d
Selvio Perez <selvio89@hotmail.com>
2024-08-22 11:37:00 -0500

    feat: basic ui

--- src-ui/App.tsx
+++ src-ui/App.tsx
@@ -1,46 +1,63 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import { invoke } from '@tauri-apps/api';
-import './App.css';
-
-async function greet(name: string): Promise<string> {
-  const result: string = await invoke('greet', { name });
-  return result;
-}
-
 function App() {
-  const [count, setCount] = useState(0);
-  const [greeting, setGreeting] = useState('Loading...');
-  useEffect(() => {
-    greet('World')
-      .then(res => setGreeting(res))
-      .catch(err => setGreeting(err.toString()));
-  });
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="px-2 pb-3 pt-4 h-screen bg-slate-950 text-lime-600 font-['Source_Code_Pro'] relative">
+      <div className="border border-lime-600 h-full pt-3">
+        <h1 className="px-2 bg-slate-950 absolute right-1/2 translate-x-1/2 top-1">
+          Gastronomy Debugger (fibonacci.uplc)
+        </h1>
+        <div className="grid grid-rows-[max-content_1fr] h-full text-sm">
+          <div className="px-2 pt-1 pb-4">
+            <div className="text-center">Step 0/448</div>
+            <div className="flex justify-between">
+              <div>
+                Current: <span className="text-blue-600">Compute</span>
+              </div>
+              <div>
+                Budget: <span className="text-blue-600">100 steps</span> (+100){" "}
+                <span className="text-blue-600">100 mem</span> (+100)
+              </div>
+              <div>
+                Next: <span className="text-blue-600">Compute</span>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 h-full border-t border-lime-600">
+            <div className="relative border-r border-lime-600 p-3">
+              <h2 className="left-2 -top-3 bg-slate-950 text-blue-600 absolute px-2 z-10">
+                Term
+              </h2>
+            </div>
+            <div className="relative">
+              <h2 className="left-2 -top-3 bg-slate-950 absolute px-2">
+                Context
+              </h2>
+              <div className="h-full grid grid-rows-2">
+                <div className="p-3">dsd</div>
+                <div className="p-3 border-t border-lime-600 relative">
+                  <h2 className="left-2 -top-3 bg-slate-950 absolute px-2">
+                    Env
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>{greeting}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src-ui/App.tsx</code> and save to test HMR
-        </p>
+      <div className="px-2 bg-slate-950 flex absolute right-1/2 translate-x-1/2 bottom-1 gap-2">
+        <div>
+          <button className="hover:underline">Next</button>{" "}
+          <span className="text-blue-600">{"<N>"}</span>
+        </div>
+        <div>
+          <button className="hover:underline">Previous</button>{" "}
+          <span className="text-blue-600">{"<P>"}</span>
+        </div>
+        <div>
+          <button className="hover:underline">Quit</button>{" "}
+          <span className="text-blue-600">{"<Q>"}</span>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
