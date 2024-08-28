9d22626a90b98af9bbf9d85fb8bea784ca6ab9b4
Pi Lanningham <pi@sundaeswap.finance>
2024-08-27 20:55:29 -0400

    Remove greetings method

--- gastronomy/src/lib.rs
+++ gastronomy/src/lib.rs
@@ -7,10 +7,6 @@ pub use execution_trace::{ExecutionTrace, Frame};
 mod execution_trace;
 pub mod uplc;
 
-pub fn greeting() -> &'static str {
-    "Hello"
-}
-
 pub fn trace_execution(filename: &Path, parameters: &[String]) -> Result<ExecutionTrace> {
     ExecutionTrace::from_file(filename, parameters)
 }
