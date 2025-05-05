7769a4ee90a53e2fe2e3d706963bc1ee3cfc1b12
yHSJ <josh@securitybot.info>
2025-05-05 11:55:42 -0400

    feat: read script overrides from cardano blueprint (CIP-57)

--- gastronomy/src/lib.rs
+++ gastronomy/src/lib.rs
@@ -1,6 +1,72 @@
-pub use execution_trace::Frame;
-
 pub mod chain_query;
 pub mod config;
 pub mod execution_trace;
 pub mod uplc;
+
+use std::{collections::HashMap, path::PathBuf};
+
+use anyhow::{Result, anyhow};
+
+//REXPORTS
+use ::uplc::tx::script_context::PlutusScript;
+use aiken_project::watch::with_project;
+pub use execution_trace::Frame;
+pub use hex;
+pub use pallas::ledger::primitives::ScriptHash;
+
+pub fn parse_script_overrides(
+    script_overrides: Vec<String>,
+) -> Result<Vec<(ScriptHash, ScriptHash)>> {
+    script_overrides
+        .into_iter()
+        .map(|key| {
+            let parts: Vec<&str> = key.split(":").collect();
+
+            if parts.len() != 2 {
+                return Err(anyhow!("invalid script-override key. Expected hash:hash"));
+            }
+
+            let from_hash = ScriptHash::from(hex::decode(parts[0])?.as_slice());
+            let to_hash = ScriptHash::from(hex::decode(parts[1])?.as_slice());
+            Ok((from_hash, to_hash))
+        })
+        .collect::<Result<Vec<_>, _>>()
+}
+
+pub fn compute_script_overrides(
+    script_overrides: Vec<(ScriptHash, ScriptHash)>,
+    maybe_blueprint: Option<PathBuf>,
+) -> Result<HashMap<ScriptHash, PlutusScript>> {
+    let mut overrides: HashMap<ScriptHash, PlutusScript> = HashMap::new();
+
+    if !script_overrides.is_empty() {
+        with_project(None, false, true, false, |project| {
+            let blueprint_path = project.blueprint_path(maybe_blueprint.as_deref());
+            let blueprint = project.blueprint(&blueprint_path)?;
+            let blueprint_validators: HashMap<ScriptHash, PlutusScript> = blueprint.into();
+
+            script_overrides
+                .iter()
+                .try_for_each::<_, Result<_, aiken_project::error::Error>>(
+                    |(from_hash, to_hash)| {
+                        overrides.insert(
+                            from_hash.clone(),
+                            blueprint_validators
+                                .get(&to_hash)
+                                .ok_or(aiken_project::error::Error::ScriptOverrideNotFound {
+                                    script_hash: to_hash.clone(),
+                                })?
+                                .clone(),
+                        );
+
+                        Ok(())
+                    },
+                )?;
+
+            Ok(())
+        })
+        .map_err(|e| anyhow!("failed to compute script overrides: {}", e))?;
+    }
+
+    Ok(overrides)
+}
